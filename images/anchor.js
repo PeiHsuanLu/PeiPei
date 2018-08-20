c870a401a971745a4ecceaea4e11f31298a4c7ef
PeiHsuanLu <PeiHsuanLu@users.noreply.github.com>
2018-08-20 20:47:08 +0800

    update1
    update1

---
+++ images/anchor.js
@@ -0,0 +1,7 @@
+// JSON Document
+$(function(){
+   $('#goto').click(function() {       
+     $('html,body').animate({scrollTop:$('#second').offset().top}, "show");
+               return false;
+           });   
+});
